a2570c8a8a42a062f926ee62f4012ca6aabfc0b1
MIKE <mikebonus777@gmail.com>
2022-07-24 14:15:28 -0400

    commit12-code-polish

--- app/src/main/java/com/lightspeed/data/api/implementation/FirstParsingImpl.kt
+++ app/src/main/java/com/lightspeed/data/api/implementation/FirstParsingImpl.kt
@@ -34,37 +34,37 @@ class FirstParsingImpl : FirstApi {
         val listOne = arrayListOf<Lightspeed>()
 
         for (i in 0 until jsonObjectLightspeed.length()) {
+
             // ID
             var id = jsonObjectLightspeed.get(i).toString()
                 .substringBefore("author")
                 .substring(7, jsonObjectLightspeed.get(i).toString()
-                    .substringBefore("author").length - 3)
-
+                .substringBefore("author").length - 3)
 
             // AUTHOR-NAME
             var author = jsonObjectLightspeed.get(i).toString()
                 .substringBefore("width")
                 .substringAfter("author")
                 .substring(3, jsonObjectLightspeed.get(i).toString()
-                        .substringBefore("width")
-                        .substringAfter("author").length - 3)
-
+                .substringBefore("width")
+                .substringAfter("author").length - 3)
 
             // DOWNLOAD-URL
             var refinedDownloadURL = refineImageLink(
                 jsonObjectLightspeed.get(i).toString()
-                    .substringAfter("download_url")
-                    .substring(3, jsonObjectLightspeed.get(i).toString()
-                    .substringAfter("download_url").length - 2)).toString()
+                .substringAfter("download_url")
+                .substring(3, jsonObjectLightspeed.get(i).toString()
+                .substringAfter("download_url").length - 2)).toString()
 
-
+            // OBJECT
             val thisLightspeed = Lightspeed(id, author, refinedDownloadURL)
             listOne.add(thisLightspeed)
         }
+
         return listOne
     }
 
-
+    // to remove 'back-slash' in the link
     private fun refineImageLink(oldString: String): String? {
         val sb = StringBuilder()
         val oldStrChar = oldString.toCharArray()
