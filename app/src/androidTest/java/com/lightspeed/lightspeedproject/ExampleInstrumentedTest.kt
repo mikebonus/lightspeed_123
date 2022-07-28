bb18233bda728f990607b3a30a6a0e4f653c6402
MIKE <mikebonus777@gmail.com>
2022-07-28 12:47:25 -0400

    commit14-polish

--- app/src/androidTest/java/com/lightspeed/lightspeedproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/lightspeed/lightspeedproject/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.codinginflow.simplecachingexample", appContext.packageName)
+        assertEquals("com.lightspeed.lightspeedproject", appContext.packageName)
     }
 }
